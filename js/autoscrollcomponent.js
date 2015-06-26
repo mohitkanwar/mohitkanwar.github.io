ded4f79a7b0c83bd36b020403ba6d48b67b0ea53
Mohit Kanwar <mkanwar@beachbody.com>
2015-06-26 12:51:49 +0530

    compressing and removing large images

--- js/autoscrollcomponent.js
+++ js/autoscrollcomponent.js
@@ -1,22 +1,39 @@
    $( document ).ready(
+   // TODO: correct for multiple usage in same page
                 function(){
                  
-                                var i=0;
+                 if($(".auto-scroll-me").length>1){
+                        for(var index=0;index<$(".auto-scroll-me").length;index++){
+                                if(!$($(".auto-scroll-me")[index]).hasClass("auto-scroll-started")){
+                                        startscrolling($($(".auto-scroll-me")[index]).children());
+                                        $($(".auto-scroll-me")[index]).addClass("auto-scroll-started");
+                                }
+                               
+                        }
+                 
+                 }
+                 else{
+                 startscrolling($(".auto-scroll-me > div"));
+                 }
+                 
+                               
+		        }
+		    );
+		    
+		    
+		    function startscrolling(array){
+		     var i=0;
                                 console.log("called with" +i);
-		                $($(".auto-scroll-me > div")[i]).effect('slide', { direction: 'right', mode: 'show' ,duration:3000});
+		                $(array[i]).effect('slide', { direction: 'right', mode: 'show' ,duration:0});
 		                setInterval(function () {
-		          	   var effect = 'slide';
-                		   var optionsright = { direction: 'right' };
-		                   var optionsleft = { direction: 'left' };
-                		   var duration = 1000;
-                                   $($(".auto-scroll-me > div")[i]).toggle(effect, optionsleft, duration,function(){
+		          	
+                                   $(array[i]).effect('slide', { direction: 'left', mode: 'hide' },2500,function(){
 		                        i++;
-		                        if(i==$(".auto-scroll-me > div").length){
+		                        if(i==array.length){
 		                            i=0;
 		                        }
 		                       
-		                        $($(".auto-scroll-me > div")[i]).toggle(effect, optionsright, duration);
+		                        $(array[i]).effect('slide', { direction: 'right', mode: 'show' },2500);
 		            	  });
-		                }, 5000); 
-		        }
-		    );
+		                }, 7000); 
+		    }
